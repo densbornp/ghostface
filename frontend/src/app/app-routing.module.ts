eb7b9cc74ae6b6a466e0b05b9060e8174b793d64
Philip Densborn <densbornp@gmail.com>
2020-11-07 18:58:06 +0100

    Added components to frontend
    Header is now sticky when scrolling.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { HomeComponent } from './home/home.component';
 import { PolicyComponent } from './policy/policy.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  //{path: 'info', component: InfoComponent},
-  //{path: 'conversion', component: ConversionComponent},
-  //{path: 'contact', component: ContactComponent},
-  {path: 'faq', component: FaqComponent},
-  {path: 'policy', component: PolicyComponent},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
-]
+  { path: '', component: HomeComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'policy', component: PolicyComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/hero/hero.component.ts
+++ frontend/src/app/hero/hero.component.ts
@@ -1,23 +1,22 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Paths } from '../paths';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
+  public homePath = Paths.PATH_HOME;
 
   @ViewChild('carousel-inner') el: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   scrollToElement(id: any) {
     let el = document.getElementById(id);
-    el.scrollIntoView({behavior: "smooth"});
+    el.scrollIntoView({ behavior: 'smooth' });
   }
-
 }
