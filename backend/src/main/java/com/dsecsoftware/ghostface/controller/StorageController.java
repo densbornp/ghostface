0fdb4e659dd9a7bf63752b1b9bf25a5a595799cc
dsecsoftware <dsecsoftware@mailbox.org>
2020-11-08 15:02:40 +0100

    Changes in backend POST method '/upload'.

--- backend/src/main/java/com/dsecsoftware/ghostface/controller/StorageController.java
+++ backend/src/main/java/com/dsecsoftware/ghostface/controller/StorageController.java
@@ -1,41 +1,45 @@
 package com.dsecsoftware.ghostface.controller;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.activation.MimetypesFileTypeMap;
+import javax.imageio.ImageIO;
 
 import com.dsecsoftware.ghostface.ParamHelper;
 import com.dsecsoftware.ghostface.PathManager;
+import com.dsecsoftware.ghostface.services.StorageService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-public class UploadController {
+public class StorageController {
+
+  private StorageService storageService;
+
+  @Autowired
+  public StorageController(StorageService storageService) {
+    this.storageService = storageService;
+  }
 
   @PostMapping("/upload")
   public ResponseEntity<Object> uploadFile(@RequestParam(ParamHelper.UPLOADED_IMAGE) MultipartFile file) {
 
-      File image = new File(PathManager.UPLOAD_DIR, file.getOriginalFilename());
-
-      // TODO non image file gets written too ?!?
-      try (InputStream in = file.getInputStream(); FileOutputStream fout = new FileOutputStream(image)) {
+      try {
+        InputStream in = file.getInputStream();
           // Check if uplaoded file is image
-          String mimetype= new MimetypesFileTypeMap().getContentType(image);
-          String type = mimetype.split("/")[0];
-          if(type.equals("image")) {
-            int line;
-            while ((line = in.read()) != -1) {
-                fout.write(line);
-            }
-            fout.flush();
+          if(ImageIO.read(in) == null) {
+            storageService.store(file);
             return ResponseEntity.status(HttpStatus.OK).build();
           } else {
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("The uploaded file is not an image!");
@@ -45,4 +49,10 @@ public class UploadController {
       }
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
   }
+
+  @GetMapping("/download")
+  public ResponseEntity<Object> downloadImage() {
+      return null;
+  }
+
 }
