93aee0ea56f1650b2d75d589ed72261d5e6e37b8
Philip Densborn <densbornp@gmail.com>
2021-01-10 11:38:10 +0100

    fixed: Angular and Java now working together
    - Routing directlink now working with /faq and /policy

--- backend/src/main/java/com/dsecsoftware/ghostface/controller/StorageController.java
+++ backend/src/main/java/com/dsecsoftware/ghostface/controller/StorageController.java
@@ -12,7 +12,6 @@ import com.dsecsoftware.ghostface.services.StorageService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class StorageController {
 
     private StorageService storageService;
